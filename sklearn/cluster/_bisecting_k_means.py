3c758557f87a0e31d98ae6b5f9aa0e633fdae75d
Aidan <ajzorbas@gmail.com>
2022-03-29 18:32:26 -0400

    Added base bisecting_k_means class

---
+++ sklearn/cluster/_bisecting_k_means.py
@@ -0,0 +1,31 @@
+
+import _kmeans
+
+class BisectingKMeans():
+
+    def __init__(self):
+        pass
+
+    def fit(X, y=None, sample_weight=None):
+        pass
+
+    def fit_predict(X, y=None, sample_weight=None):
+        pass
+
+    def fit_transform(X, y=None, sample_weight=None):
+        pass
+    
+    def get_params(deep=True):
+        pass
+
+    def set_params(**params):
+        pass
+
+    def score(X, y=None, sample_weight=None):
+        pass
+
+    def transform(X):
+        pass
+
+    def predict(X, sample_weight=None):
+        pass
